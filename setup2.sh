5ca8ad72ad372d6bbad2711f1731155b684de48a
Rehan Durrani <rdurrani@berkeley.edu>
2017-09-07 20:19:14 -0700

    Switch order of anaconda and python3 install

--- setup2.sh
+++ setup2.sh
@@ -5,6 +5,7 @@ brew install wget
 brew install git
 brew cask install java
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
+echo 'Python3 has been installed, but can not be aliased to python, since some apps expect python to be aliased to Python2.7'
 echo 'Would you like to install Anaconda (y)es or (n)o?'
 read anaconda
 if [[ $anaconda = 'y' ]]
@@ -26,8 +27,9 @@ then
     open Anaconda$anaconda_distro-$a_version-MacOSX-x86_64.pkg
   fi
 fi
-echo 'Python3 has been installed, but can not be aliased to python, since some apps expect python to be aliased to Python2.7'
-echo 'Would you like to alias Python3? [y]es or [n]o.'
+echo 'Would you like to install ZSH? (y)es or (n)o.'
+
+echo 'Would you like to alias Python3? (y)es or (n)o.'
 echo 'source ~/.git-completion.bash' >> ~/.bashrc
 read aliaspython
 if [[ $aliaspython = 'y' ]]
@@ -36,7 +38,7 @@ then
   read pythonalias
   echo "alias $pythonalias=\"python3\"" >> ~/.bash_profile
 fi
-echo 'Would you like to alias git checkout? [y]es or [n]o.'
+echo 'Would you like to alias git checkout? (y)es or (n)o.'
 read aliasgitcheckout
 if [[ $aliasgitcheckout = 'y' ]]
 then
@@ -45,7 +47,7 @@ then
   echo "alias $gitcheckoutalias=\"git checkout\"" >> ~/.bash_profile
   echo "__git_complete $gitcheckoutalias _git_checkout" >> ~/.bashrc
 fi
-echo 'Would you like to alias git status? [y]es or [n]o.'
+echo 'Would you like to alias git status? (y)es or (n)o.'
 read aliasgitstatus
 if [[ $aliasgitstatus = 'y' ]]
 then
@@ -53,7 +55,7 @@ then
   read gitstatusalias
   echo "alias $gitstatusalias=\"git status\"" >> ~/.bash_profile
 fi
-echo 'Would you like to create a function to push new local branches upstream with the appropriate name automatically when called? [y]es or [n]o.'
+echo 'Would you like to create a function to push new local branches upstream with the appropriate name automatically when called? (y)es or (n)o.'
 read addgpo
 if [[ $addgpo = 'y' ]]
 then
@@ -61,7 +63,7 @@ then
   read functionname
   echo "function $functionname() { branch=\$(git rev-parse --abbrev-ref HEAD); git push --set-upstream origin \$branch;}" >> ~/.bashrc
 fi
-echo 'Would you like to create a function to allow you to commit whatever follows your command call, without quotes? [y]es or [n]o.'
+echo 'Would you like to create a function to allow you to commit whatever follows your command call, without quotes? (y)es or (n)o.'
 read addcommit
 if [[ $addcommit = 'y' ]]
 then
@@ -69,7 +71,7 @@ then
   read functionname
   echo "function $functionname() { git add .; git commit -m \"\$*\";}" >> ~/.bashrc
 fi
-echo 'Would you like to create a function to git clone whatever is in your clipboard? [y]es or [n]o.'
+echo 'Would you like to create a function to git clone whatever is in your clipboard? (y)es or (n)o.'
 read addgclone
 if [[ $addgclone = 'y' ]]
 then
@@ -77,7 +79,7 @@ then
   read functionname
   echo "function $functionname() { git clone \`pbpaste\`;}" >> ~/.bashrc
 fi
-echo 'Would you like to add a function to create a new branch with the specified name? [y]es or [n]o.'
+echo 'Would you like to add a function to create a new branch with the specified name? (y)es or (n)o.'
 read addnewbranch
 if [[ $addnewbranch = 'y' ]]
 then
